8c12f6d0a1ad69b38a60bea9ea8a66c48b5513d2
alessandro <alessandro.olivo@elettra.eu>
2025-08-14 15:12:47 +0200

    Add xrd2 beamlineaction

---
+++ mxcubecore/HardwareObjects/ELETTRA/XRD2/XRD2BeamlineActions.py
@@ -0,0 +1,31 @@
+import logging
+
+from pydantic.v1 import (
+    BaseModel,
+    Field,
+)
+
+from mxcubecore.HardwareObjects.BeamlineActions import (
+    AnnotatedCommand,
+    BeamlineActions,
+)
+import mxcubecore.HardwareRepository as HWR
+
+
+class SampleLocation(BaseModel):
+    basket_position: int = Field(gt=0, lt=17)
+    sample_position: int = Field(gt=0, lt=13)
+
+
+class SetLoadedSample(AnnotatedCommand):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+    def set_loaded_sample(self, data: SampleLocation) -> None:
+        logging.getLogger("user_level_log").info("Forcing loaded sample to %s:%s",
+                                                 data.sample_position, data.sample_position)
+        HWR.beamline.sample_changer.force_loaded_sample(f"{data.sample_position}:{data.sample_position}")
+
+
+class XRD2BeamlineActions(BeamlineActions):
+    pass
